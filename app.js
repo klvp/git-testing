df880d7b5dfc9df910bbed6e03a2889fb1597989
Lakshmi_Vara_Prasad Kuna <klvpfeb98@gmail.com>
2022-11-25 08:30:35 +0530

    feature 1 added

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 /** @format */
 
 console.log("I am Full stack developer");
+
+function user({ name, age, role }) {
+  return `${name} is ${age} years old and working as ${role}`;
+}
+
+console.log(user({ name: "klvp", age: 24, role: "SDE 1" }));
